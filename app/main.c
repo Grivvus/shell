bfd04208199838a289a1f7a7942ffb82a35a9a7a
Arseniy <kruchik.a04@gmail.com>
2024-12-05 17:30:34 +0400

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,12 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-   printf("$ ");
-   fflush(stdout);
-
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    int num_of_builins = 2;
+    char builtins[2][4] = {"echo", "exit"};
+    printf("$ ");
+    fflush(stdout);
+    
+    char input[100];
+    fgets(input, 100, stdin);
+    int len = strlen(input);
+    input[len-1] = '\0';
+    for (int i = 0; i < num_of_builins; i++){
+        if (strcmp((char*)input, (char*)builtins[i]) == 0){
+            printf("do something\n");
+            exit(0);
+        }
+    }
+    
+    printf("%s: command not found\n", (char*)input);
+    exit(1);
+    return 0;
 }
